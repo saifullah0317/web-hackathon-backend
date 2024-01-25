abcb0aa22a4972ad2662ce4147119e177c2ff782
saifullah0317 <saifullaharshad999@gmail.com>
2024-01-25 15:22:08 +0500

    checked-cors

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,11 @@ import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: true,
-  //   // credentials: true,
-  // });
   app.enableCors({
     origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
-    credentials: true, // Enable credentials (cookies, authorization headers) cross-origin
+    credentials: true,
   });
   app.use(cookieParser());
-  await app.listen(8000);
+  await app.listen(3002);
 }
 bootstrap();
