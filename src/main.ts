e323ba101fc8ad1de26428a5784c3d368f1b7465
saifullah0317 <saifullaharshad999@gmail.com>
2024-01-25 14:58:57 +0500

    checked-cors

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,20 @@ import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors: true});
   // app.enableCors({
   //   origin: true,
   //   // credentials: true,
   // });
-  app.enableCors({
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // allowed headers
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
-    // headers exposed to the client
-    exposedHeaders: ['Authorization'],
-    credentials: true, // Enable credentials (cookies, authorization headers) cross-origin
-  });
+  // app.enableCors({
+  //   origin: true,
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   // allowed headers
+  //   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
+  //   // headers exposed to the client
+  //   exposedHeaders: ['Authorization'],
+  //   credentials: true, // Enable credentials (cookies, authorization headers) cross-origin
+  // });
   app.use(cookieParser());
   await app.listen(8000);
 }
