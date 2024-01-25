2ae569ae0a37a980fa58e13147b00666bdf6b397
saifullah0317 <saifullaharshad999@gmail.com>
2024-01-25 14:43:17 +0500

    checked-cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: ['https://web-hackathon-backend.vercel.app'],
     // credentials: true,
   });
   // app.enableCors();
